c07150b893a7c1791abbcdf24f101763c09d03b7
Blackbird12356 <moneyup1925@gmail.com>
2025-08-03 20:05:44 +0500

    Добавил передвижение но есть проблеммы с мультиплером
    Есть проблемы с мультиплеером altarun в скрипте аватар, при подключение 2 игрока 2 игрок ходит с 1 игроком надо исправить я устал просто

---
+++ Assets/scripts/FPController.cs
@@ -0,0 +1,164 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class F : MonoBehaviour
+{
+    [Header("Movement Parameters")]
+    public float MaxSpeed => SprintInput ? SprintSpeed : WalkSpeed;
+    public float Acceleration = 15f;
+
+    [SerializeField] float WalkSpeed = 3.5f;
+    [SerializeField] float SprintSpeed = 8f;
+
+    [Space(15)]
+    [SerializeField] float JumpHeight = 2f;
+
+    public bool Sprinting
+    {
+        get
+        {
+            return SprintInput && CurrentSpeed > 0.1f;
+        }
+    }
+
+    [Header("Looking Parameters")]
+    public Vector2 LookSensitivity = new Vector2(0.1f, 0.1f);
+
+    public float PitchLimit = 85f;
+
+    [SerializeField] float currentPitch = 0f;
+
+    public float CurrentPitch
+    {
+        get => currentPitch;
+
+        set
+        {
+            currentPitch = Mathf.Clamp(value, -PitchLimit, PitchLimit);
+        }
+    }
+
+    [Header("Camera Parameters")]
+    [SerializeField] float CameraNormalFOV = 60f;
+    [SerializeField] float CameraSprintFOV = 80f;
+    [SerializeField] float CameraFOVSmoothing = 1f;
+
+    float TargetCameraFOV
+    {
+        get
+        {
+            return Sprinting ? CameraSprintFOV : CameraNormalFOV;
+        }
+    }
+
+    [Header("Physics Parameters")]
+    [SerializeField] float GravityScale = 3f;
+
+    public float VerticalVelocity = 0f;
+
+    public Vector3 CurrentVelocity { get; private set; }
+    public float CurrentSpeed { get; private set; }
+
+    public bool IsGrounded => characterController.isGrounded;
+
+    [Header("Input")]
+    public Vector2 MoveInput;
+    public Vector2 LookInput;
+    public bool SprintInput;
+
+    [Header("Components")]
+    [SerializeField] CinemachineCamera fpCamera;
+    [SerializeField] CharacterController characterController;
+
+    #region Unity Methods
+
+    void OnValidate()
+    {
+      if (characterController == null)
+        {
+            characterController = GetComponent<CharacterController>();
+        }  
+    }
+
+
+    void Update()
+    {
+        MoveUpdate();
+        LookUpdate();
+        CameraUpdate();
+    }
+
+    #endregion
+
+    #region Controller Methods
+
+    public void TryJump()
+    {
+        if (IsGrounded == false)
+        {
+            return;
+        }
+
+        VerticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y * GravityScale);
+    }
+
+    void MoveUpdate()
+    {
+        Vector3 motion = transform.forward * MoveInput.y + transform.right * MoveInput.x;
+        motion.y = 0f;
+        motion.Normalize();
+
+        if (motion.sqrMagnitude >= 0.01f)
+        {
+            CurrentVelocity = Vector3.MoveTowards(CurrentVelocity, motion * MaxSpeed, Acceleration * Time.deltaTime);
+        }
+        else
+        {
+            CurrentVelocity = Vector3.MoveTowards(CurrentVelocity, Vector3.zero, Acceleration * Time.deltaTime);
+        }
+
+        if (IsGrounded && VerticalVelocity <= 0.01f)
+        {
+            VerticalVelocity = -3f;
+        }
+        else
+        {
+            VerticalVelocity += Physics.gravity.y * GravityScale * Time.deltaTime;
+        }
+
+        Vector3 fullVelocity = new Vector3(CurrentVelocity.x, VerticalVelocity, CurrentVelocity.z);
+
+        characterController.Move(fullVelocity * Time.deltaTime);
+
+        CurrentSpeed = CurrentVelocity.magnitude;
+    }
+
+    void LookUpdate()
+    {
+        Vector2 input = new Vector2(LookInput.x * LookSensitivity.x, LookInput.y * LookSensitivity.y);
+
+        CurrentPitch -= input.y;
+
+        fpCamera.transform.localRotation = Quaternion.Euler(CurrentPitch, 0f, 0f);
+
+        transform.Rotate(Vector3.up * input.x);
+    }
+
+    void CameraUpdate()
+    {
+        float targetFOV = CameraNormalFOV;
+
+        if (Sprinting)
+        {
+            float speedRatio = CurrentSpeed / SprintSpeed;
+
+            targetFOV = Mathf.Lerp(CameraNormalFOV, CameraSprintFOV, speedRatio);
+        }
+
+        fpCamera.Lens.FieldOfView = Mathf.Lerp(fpCamera.Lens.FieldOfView, targetFOV, CameraFOVSmoothing * Time.deltaTime);
+    }
+
+    #endregion
+}
---
+++ Assets/scripts/FPPlayer.cs
@@ -0,0 +1,52 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(F))]
+public class FPPlayer : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] F FPController;
+
+    #region Input Handling
+
+    void OnMove(InputValue value)
+    {
+        FPController.MoveInput = value.Get<Vector2>();
+    }
+
+    void OnLook(InputValue value)
+    {
+        FPController.LookInput = value.Get<Vector2>();
+    }
+
+    void OnSprint(InputValue value)
+    {
+        FPController.SprintInput = value.isPressed;
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            FPController.TryJump();
+        }
+    }
+
+    #endregion
+
+    #region Unity Methods
+
+    void OnValidate()
+    {
+        if (FPController == null) FPController = GetComponent<F>();    
+    }
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    #endregion
+}
